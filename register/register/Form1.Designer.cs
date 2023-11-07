68c8099084990aa6c094903d614380aba543906b
G-ley <146834592+G-ley@users.noreply.github.com>
2023-11-07 19:59:18 +0800

    register

---
+++ register/register/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace register
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblreg = new System.Windows.Forms.Label();
+            this.lblname = new System.Windows.Forms.Label();
+            this.lblpass = new System.Windows.Forms.Label();
+            this.lblcpass = new System.Windows.Forms.Label();
+            this.lblemail = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.txtbox1 = new System.Windows.Forms.TextBox();
+            this.txtbox2 = new System.Windows.Forms.TextBox();
+            this.txtbox3 = new System.Windows.Forms.TextBox();
+            this.txtbox4 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblreg
+            // 
+            this.lblreg.AutoSize = true;
+            this.lblreg.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblreg.Location = new System.Drawing.Point(410, 53);
+            this.lblreg.Name = "lblreg";
+            this.lblreg.Size = new System.Drawing.Size(160, 31);
+            this.lblreg.TabIndex = 0;
+            this.lblreg.Text = "Registration";
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblname.Location = new System.Drawing.Point(342, 119);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(55, 20);
+            this.lblname.TabIndex = 1;
+            this.lblname.Text = "Name:";
+            // 
+            // lblpass
+            // 
+            this.lblpass.AutoSize = true;
+            this.lblpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpass.Location = new System.Drawing.Point(289, 165);
+            this.lblpass.Name = "lblpass";
+            this.lblpass.Size = new System.Drawing.Size(117, 20);
+            this.lblpass.TabIndex = 2;
+            this.lblpass.Text = "New Password:";
+            this.lblpass.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblcpass
+            // 
+            this.lblcpass.AutoSize = true;
+            this.lblcpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcpass.Location = new System.Drawing.Point(265, 215);
+            this.lblcpass.Name = "lblcpass";
+            this.lblcpass.Size = new System.Drawing.Size(141, 20);
+            this.lblcpass.TabIndex = 3;
+            this.lblcpass.Text = "Confirm Password:";
+            // 
+            // lblemail
+            // 
+            this.lblemail.AutoSize = true;
+            this.lblemail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblemail.Location = new System.Drawing.Point(335, 263);
+            this.lblemail.Name = "lblemail";
+            this.lblemail.Size = new System.Drawing.Size(62, 20);
+            this.lblemail.TabIndex = 4;
+            this.lblemail.Text = "EMAIL:";
+            this.lblemail.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(412, 313);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(158, 28);
+            this.btn1.TabIndex = 5;
+            this.btn1.Text = "Confirm";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // txtbox1
+            // 
+            this.txtbox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox1.Location = new System.Drawing.Point(412, 119);
+            this.txtbox1.Name = "txtbox1";
+            this.txtbox1.Size = new System.Drawing.Size(158, 22);
+            this.txtbox1.TabIndex = 6;
+            this.txtbox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtbox2
+            // 
+            this.txtbox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox2.Location = new System.Drawing.Point(412, 165);
+            this.txtbox2.Name = "txtbox2";
+            this.txtbox2.Size = new System.Drawing.Size(158, 22);
+            this.txtbox2.TabIndex = 7;
+            // 
+            // txtbox3
+            // 
+            this.txtbox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox3.Location = new System.Drawing.Point(412, 215);
+            this.txtbox3.Name = "txtbox3";
+            this.txtbox3.Size = new System.Drawing.Size(158, 22);
+            this.txtbox3.TabIndex = 8;
+            // 
+            // txtbox4
+            // 
+            this.txtbox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox4.Location = new System.Drawing.Point(412, 261);
+            this.txtbox4.Name = "txtbox4";
+            this.txtbox4.Size = new System.Drawing.Size(158, 22);
+            this.txtbox4.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::register.Properties.Resources.download__2_;
+            this.pictureBox1.Location = new System.Drawing.Point(-15, -21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(274, 509);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtbox4);
+            this.Controls.Add(this.txtbox3);
+            this.Controls.Add(this.txtbox2);
+            this.Controls.Add(this.txtbox1);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lblemail);
+            this.Controls.Add(this.lblcpass);
+            this.Controls.Add(this.lblpass);
+            this.Controls.Add(this.lblname);
+            this.Controls.Add(this.lblreg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblreg;
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.Label lblpass;
+        private System.Windows.Forms.Label lblcpass;
+        private System.Windows.Forms.Label lblemail;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox txtbox1;
+        private System.Windows.Forms.TextBox txtbox2;
+        private System.Windows.Forms.TextBox txtbox3;
+        private System.Windows.Forms.TextBox txtbox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
